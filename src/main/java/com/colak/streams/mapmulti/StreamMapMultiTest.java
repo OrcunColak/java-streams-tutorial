1e5da3b5f8bba3793eb3b72a1418cf81f1df3357
Orçun Çolak <orcun.colak@hazelcast.com>
2025-01-26 20:36:56 +0300

    mapmulti example

--- src/main/java/com/colak/streams/mapmulti/StreamMapMultiTest.java
+++ src/main/java/com/colak/streams/mapmulti/StreamMapMultiTest.java
@@ -9,6 +9,31 @@ class StreamMapMultiTest {
 
     public static void main() {
 
+        filterAndMap();
+        upperCaseLowerCase();
+    }
+
+    // extract only the even integers from the list.
+    // when we call consumer.accept() from mapMulti the element is accepted
+    private static void filterAndMap() {
+        List<String> strings = List.of("1", "error", "42", "3", "banana", "4");
+        var evenNumbers = strings.stream()
+                .<Integer>mapMulti((line, consumer) -> {
+                    try {
+                        int number = Integer.parseInt(line);
+                        if (number % 2 == 0) {
+                            consumer.accept(number);
+                        }
+                    } catch (NumberFormatException ignored) {
+                    }
+                })
+                .toList();
+        // List : [A, a, B, b, C, c]
+        log.info("evenNumbers : {}", evenNumbers);
+    }
+
+    // when we call consumer.accept() from mapMulti the element is accepted
+    private static void upperCaseLowerCase() {
         List<String> strings = List.of("a", "b", "c");
 
         List<String> result = strings.stream()
@@ -21,6 +46,8 @@ class StreamMapMultiTest {
         // List : [A, a, B, b, C, c]
         log.info("List : {}", result);
     }
+
+
 }
 
 
